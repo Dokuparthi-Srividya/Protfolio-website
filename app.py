67fc10a17ad0dac79c3d69d19273fbf61a8b9d1b
Srividya <dokuparthisrividya@gmail.com>
2025-07-01 14:26:07 +0530

    Removed unused comment

--- app.py
+++ app.py
@@ -12,13 +12,6 @@ mydb=mysql.connector.connect(
     database=os.getenv("DB_NAME")
 )
 
-# ✅ MySQL DB connection config
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="root",  # 🔒 Replace with your actual MySQL password
-#     database="portfolio_db"
-# )
 
 @app.route('/')
 def home():
@@ -63,30 +56,4 @@ def download_resume():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# from flask import Flask, render_template,send_file,request
-# app=Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-# @app.route('/projects')
-# def projects():
-#     return render_template('projects.html')
-# @app.route('/contact',
-# methods=['GET','POST'])
-# def contact():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         message = request.form['message']
-#         print(f"Message from {name}({email}): {message}")
-#         return render_template('contact.html', success=True)
-#     return render_template('contact.html')
-# @app.route('/resume')
-# def download_resume():
-#     return send_file('resume.pdf',as_attachment=True)
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
